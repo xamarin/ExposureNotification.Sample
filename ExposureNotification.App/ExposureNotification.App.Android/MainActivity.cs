9cddc13e4d39d2bb9aca4995195090e4a129636c
James Montemagno <james.montemagno@gmail.com>
2020-05-13 13:19:30 -0700

    Remove files we don't need anymore

--- ExposureNotification.App/ExposureNotification.App.Android/MainActivity.cs
+++ ExposureNotification.App/ExposureNotification.App.Android/MainActivity.cs
@@ -18,8 +18,6 @@ namespace ExposureNotification.App.Droid
 	{
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
-			//TabLayoutResource = Resource.Layout.Tabbar;
-			//ToolbarResource = Resource.Layout.Toolbar;
 
 			base.OnCreate(savedInstanceState);
 
