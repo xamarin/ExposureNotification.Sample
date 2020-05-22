7a6ce28e54fce44e6a4364a2a64261a31f14b2ab
Matthew Leibowitz <mattleibow@live.com>
2020-05-22 18:03:42 +0200

    Android testing work, now to use it

--- ExposureNotification.Backend.Functions.Tests/AndroidVerifyTests.cs
+++ ExposureNotification.Backend.Functions.Tests/AndroidVerifyTests.cs
@@ -2,30 +2,34 @@
 using System.IO;
 using ExposureNotification.Backend.DeviceVerification;
 using ExposureNotification.Backend.Network;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace ExposureNotification.Backend.Functions.Tests
 {
 	public class AndroidVerifyTests
 	{
-		const string appPackageName = "com.google.android.apps.exposurenotification";
+		static readonly SelfDiagnosisSubmission actualSubmission = 
+			JsonConvert.DeserializeObject<SelfDiagnosisSubmission>(File.ReadAllText("TestAssets/submission.json"));
 
-		static readonly string payload = File.ReadAllText("TestAssets/test.payload");
+		const string cleartext =
+			"com.companyname.ExposureNotification.app|" +
+			"A8sh1Z5hB7hFKejbzwclnA==.2649983.3.2,Qm8HISNU/bdI+9o1/4ZHZw==.2650127.0.6|" +
+			"DEFAULT|" +
+			"POSITIVE_TEST_123456";
 
-		const string cleartext = "com.google.android.apps.exposurenotification|QLIvVheW9p6JiTx4pslesg==.2649312.144.1,UW7pkDKfLbfLs8LveFyY3w==.2649168.144.1|US|POSITIVE_TEST_123456";
-		const string sha256 = "VLIju4jpickw6QDy/poqNlVd/0aIsJlNclAIh2jBz1M=";
+		const string sha256 = "pnpQ6KkSqZ+y0yJIxKc9eqNXIM8olKZ5/rV0mxvIDWg=";
 
 		static readonly SelfDiagnosisSubmission submission = new SelfDiagnosisSubmission
 		{
-			AppPackageName = appPackageName,
-			DeviceVerificationPayload = payload,
+			AppPackageName = "com.companyname.ExposureNotification.app",
 			Platform = "android",
-			Regions = new[] { "US" },
+			Regions = new[] { "default" },
 			VerificationPayload = "POSITIVE_TEST_123456",
 			Keys =
 			{
-				new ExposureKey { Key = "QLIvVheW9p6JiTx4pslesg==", RollingStart = 2649312, RollingDuration = 144, TransmissionRisk = 1 },
-				new ExposureKey { Key = "UW7pkDKfLbfLs8LveFyY3w==", RollingStart = 2649168, RollingDuration = 144, TransmissionRisk = 1 },
+				new ExposureKey { Key = "Qm8HISNU/bdI+9o1/4ZHZw==", RollingStart = 2650127, RollingDuration = 0, TransmissionRisk = 6 },
+				new ExposureKey { Key = "A8sh1Z5hB7hFKejbzwclnA==", RollingStart = 2649983, RollingDuration = 3, TransmissionRisk = 2 },
 			}
 		};
 
@@ -45,13 +49,13 @@ namespace ExposureNotification.Backend.Functions.Tests
 			Assert.Equal(sha256, Convert.ToBase64String(nonce));
 		}
 
-		//[Fact]
-		//public void VerifyPayloadTest()
-		//{
-		//	var claims = AndroidVerify.VerifyPayload(payload);
-		//	var nonce = submission.GetAndroidNonce();
+		[Fact]
+		public void VerifyPayloadTest()
+		{
+			var claims = AndroidVerify.VerifyPayload(actualSubmission.DeviceVerificationPayload);
+			var nonce = submission.GetAndroidNonce();
 
-		//	Assert.Equal(Convert.ToBase64String(nonce), Convert.ToBase64String(claims.Nonce));
-		//}
+			Assert.Equal(Convert.ToBase64String(nonce), Convert.ToBase64String(claims.Nonce));
+		}
 	}
 }
--- ExposureNotification.Backend.Functions.Tests/TestSigner.cs
+++ ExposureNotification.Backend.Functions.Tests/TestSigner.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using System.Threading.Tasks;
 using ExposureNotification.Backend.Database;
 using ExposureNotification.Backend.Signing;
@@ -12,11 +9,9 @@ namespace ExposureNotification.Backend.Functions.Tests
 {
 	public class TestSigner : ISigner
 	{
-		// TODO: You need to sign this with the key and mechanism
-		// that google/apple shared with you
 		public Task<byte[]> GenerateSignatureAsync(byte[] contents, DbSignerInfo signerInfo)
 		{
-			var pfx = File.ReadAllBytes("../../../../sample-ecdsa-p256-cert.pfx");
+			var pfx = File.ReadAllBytes("TestAssets/sample-ecdsa-p256-cert.pfx");
 
 			// Turn this into a certificate object
 			var keyVaultCert = new X509Certificate2(pfx);
