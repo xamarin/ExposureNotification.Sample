ee187e8892c7853ee0cb27d864505a2800ed886b
Jonathan Dick <jondick@gmail.com>
2020-05-21 22:56:40 -0400

    First pass at adding an actual signature implementation

--- ExposureNotification.Backend.Functions/Signing/Signer.cs
+++ ExposureNotification.Backend.Functions/Signing/Signer.cs
@@ -1,4 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using ExposureNotification.Backend.Proto;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 
 namespace ExposureNotification.Backend.Signing
 {
@@ -6,7 +12,31 @@ namespace ExposureNotification.Backend.Signing
 	{
 		// TODO: You need to sign this with the key and mechanism
 		// that google/apple shared with you
-		public Task<byte[]> GenerateSignatureAsync(byte[] contents)
-			=> Task.FromResult(new byte[0]);
+		public async Task<byte[]> GenerateSignatureAsync(byte[] contents)
+		{
+			// Get the configured secret ID
+			var keyVaultSecretId = Environment.GetEnvironmentVariable("SigningKeyVaultSecretId", EnvironmentVariableTarget.Process);
+
+			// Use a built in token provider which works in azure functions if the service/app identity is granted access to the key vault
+			var azureServiceTokenProvider = new AzureServiceTokenProvider();
+
+			// Create a key vault client instance with the token provider
+			var keyVault = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+			// Get the secret specified in the app config
+			// This is actually am Elliptic Curve certificate (ECDSA) with a P-256 curve
+			var keyVaultSecret = await keyVault.GetSecretAsync(keyVaultSecretId);
+
+			// Turn this into a certificate object
+			var keyVaultCert = new X509Certificate2(Convert.FromBase64String(keyVaultSecret.Value));
+
+			// Get the private key to use for creating the signature
+			var ecdsaPrivateKey = keyVaultCert.GetECDsaPrivateKey();
+
+			// Create our signature based on the contents
+			var signature = ecdsaPrivateKey.SignData(contents, HashAlgorithmName.SHA256);
+
+			return signature;
+		}
 	}
 }
